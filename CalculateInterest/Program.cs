b4bf353c111971a11e3f5a880d1fd64824038611
hoppenic <hoppenic@gmail.com>
2018-08-21 08:46:24 -0500

    Add project files.

---
+++ CalculateInterest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CalculateInterest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //data needed for table
+            decimal principal = 0M;
+            decimal interest = 0M;
+            decimal duration = 0M;
+
+            //verify data by mirroring it back to the user
+            Console.WriteLine(); //skip a line
+            Console.WriteLine("Principal = " + principal);
+            Console.WriteLine("Interest = " + interest + "%");
+            Console.WriteLine("Duration = " + duration + " years");
+            Console.WriteLine();  //skip a line
+
+
+
+
+
+
+        }
+    }
+}
